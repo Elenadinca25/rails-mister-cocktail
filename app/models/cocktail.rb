541be2883d097d6a063f2461d3c03fdfaf459e86
Elena Dinca <elenadinca25@gmail.com>
2020-09-04 15:21:46 +0100

    added some styling

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,12 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, :through => :doses
   validates_uniqueness_of :name
   has_one_attached :photo
+
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name_and_location,
+  #   against: [:name],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
+

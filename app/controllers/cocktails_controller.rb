9ffe35664ad218144279349a86f092f4f0a87e92
Elena Dinca <elenadinca25@gmail.com>
2020-09-05 15:59:07 +0100

    added favicon

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -19,7 +19,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to root_path
+      redirect_to cocktail_path
     else
       render :new
     end
